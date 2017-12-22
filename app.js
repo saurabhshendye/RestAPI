deb143289de9af1f70cfcbd5461e13480a7e3c17
saurabhs <saurabh.shendye@gmail.com>
2017-12-22 01:12:48 -0700

    API working

--- app.js
+++ app.js
@@ -2,9 +2,42 @@ const express = require('express')
 const mysql = require('mysql')
 const bodyParser = require('body-parser')
 const app = express()
+const TABLE_NAME = 'Air_Quality'
+
+app.use(bodyParser.json())
+
+app.use(bodyParser.urlencoded({
+   extended: true
+}));
 
 var fs = require("fs"),
     json;
+
+// config.json is in application root
+json = readJsonFileSync('config.json')
+// console.log('username: ', json.mysql.username);
+var connection = mySqlConnect()
+
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(5001, () => console.log('listening on port 5001!'))
+
+app.get('/api/sum', (req, res) => res.send('Sum is 15'))
+
+app.post('/api/Analyzer/putData', putData)
+
+app.post('/', putData)
+
+function putData(req, res, next){
+  console.log('Connected');
+  insertIntoMysql(req.body)
+  next()
+}
+
+function testFunction(req, res1){
+  console.log(req.body)
+  var testvar = 'variable'
+  // res1.send('Test Successful!')
+}
 
 function readJsonFileSync(filepath, encoding){
 
@@ -15,39 +48,6 @@ function readJsonFileSync(filepath, encoding){
     return JSON.parse(file)
 }
 
-// config.json is in application root
-json = readJsonFileSync('config.json')
-// console.log('username: ', json.mysql.username);
-mySqlConnect()
-
-
-// connection.query('SELECT * FROM Air_Quality', function (err, rows, fields) {
-//   if (err) throw err
-//
-//   console.log('The solution is: ', rows[0].CO)
-// })
-
-// connection.end()
-
-
-
-
-app.get('/', (req, res) => res.send('Hello World!'))
-app.get('/api/sum', (req, res) => res.send('Sum is 15'))
-
-app.post('/Analyzer/api/putData', putData)
-
-function putData(req, res){
-  console.log(req.body);
-  next()
-}
-
-function testFunction(req, res1){
-  console.log(req.body)
-  var testvar = 'variable'
-  // res1.send('Test Successful!')
-}
-
 function mySqlConnect(){
   var connection = mysql.createConnection({
     host     : json.mysql.hostname,
@@ -56,12 +56,20 @@ function mySqlConnect(){
     database : json.mysql.database
   });
 
-  connection.connect()
+  connection.connect(function(err) {
+  if (err) throw err
+  console.log('You are now connected...')})
+
+  return connection
 }
 
 function insertIntoMysql(data) {
-
+  console.log(data);
+  // jsonData = JSON.parse(data)
+  console.log(data.CO);
+  connection.query('INSERT INTO ' + TABLE_NAME + '(CO, LPG, SMOKE, PM10, PM25) VALUES('
+                  + data.CO + ', ' + data.LPG + ', ' + data.Smoke + ', ' + data.Particles10 + ', '
+                  + data.Particles25 + ')' , function(err, result){
+                    if (err) throw err
+                  })
 }
-
-
-// app.listen(5000, () => console.log('Example app listening on port 5000!'))
