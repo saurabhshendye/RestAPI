028ca3bf6279c00004e57339e0ba8669ce33a455
saurabhs <saurabh.shendye@gmail.com>
2018-01-03 18:12:06 -0700

    Added config.json file.  Code reformatting in app.js

--- app.js
+++ app.js
@@ -13,30 +13,23 @@ app.use(bodyParser.urlencoded({
 var fs = require("fs"),
     json;
 
-// config.json is in application root
+// config.json contains credentials for the database
+// Need to read this file synchronously 
 json = readJsonFileSync('config.json')
-// console.log('username: ', json.mysql.username);
 var connection = mySqlConnect()
 
-// app.get('/', (req, res) => res.send('Hello World!'))
+// Listen on port 5001
 app.listen(5001, () => console.log('listening on port 5001!'))
 
-app.get('/api/sum', (req, res) => res.send('Sum is 15'))
 
 app.post('/api/Analyzer/putData', putData)
 
-app.post('/', putData)
-
+// Callback function for app.post
 function putData(req, res, next){
-  console.log('Connected');
-  insertIntoMysql(req.body)
+  console.log('Connected')
+  insertIntoMysql(req.body, res)
+  res.sendStatus(200)
   next()
-}
-
-function testFunction(req, res1){
-  console.log(req.body)
-  var testvar = 'variable'
-  // res1.send('Test Successful!')
 }
 
 function readJsonFileSync(filepath, encoding){
@@ -58,18 +51,18 @@ function mySqlConnect(){
 
   connection.connect(function(err) {
   if (err) throw err
-  console.log('You are now connected...')})
 
+  console.log('You are now connected...')})
   return connection
 }
 
-function insertIntoMysql(data) {
-  console.log(data);
-  // jsonData = JSON.parse(data)
-  console.log(data.CO);
+function insertIntoMysql(data, res) {
   connection.query('INSERT INTO ' + TABLE_NAME + '(CO, LPG, SMOKE, PM10, PM25) VALUES('
                   + data.CO + ', ' + data.LPG + ', ' + data.Smoke + ', ' + data.Particles10 + ', '
                   + data.Particles25 + ')' , function(err, result){
-                    if (err) throw err
+                    if (err){
+                      console.log(err)
+                      res.sendStatus(500)
+                    } 
                   })
 }
